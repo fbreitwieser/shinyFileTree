c68ad1d48ad273ef4c1657d0a4b0eec1e65a20b4
fbreitw1@jhu.edu <Florian Breitwieser>
2016-05-07 10:58:45 -0400

    Expanded demo app to include some of the recent modifications

--- inst/shinyapp/app.R
+++ inst/shinyapp/app.R
@@ -2,6 +2,9 @@ library(shiny)
 library(shinyFileTree)
 
 ui = shinyUI(fluidPage(
+  textInput("dir",label = "Directory", value = system.file(package="shinyFileTree")),
+  textInput("ext",label = "Extension", value = ""),
+  checkboxInput("hide_empty_dirs",label = "Hide empty directories", value = TRUE),
   checkboxGroupInput("plugins","Plugins", sort(c("checkbox",
                  "contextmenu",
                  "dnd",
@@ -14,15 +17,20 @@ ui = shinyUI(fluidPage(
                  "wholerow",
                  "changed",
                  "conditionalselect"
-  )), selected = c("checkbox", "types")),
-  verbatimTextOutput("msg"),
-  "",
-  shinyFileTreeOutput('jstree')
+  )), selected = c("checkbox", "types"), inline = TRUE),
+  shinyFileTreeOutput('jstree'),
+  "Selected files:",
+  verbatimTextOutput("msg")
 ))
 
 server = function(input, output) {
   output$jstree <- renderShinyFileTree(
-    shinyFileTree(get_list_from_directory(system.file(package="shinyFileTree")),
+    shinyFileTree(list(text=input$dir,
+                       type="directory",
+                       state=list(opened=TRUE),
+                       children=get_list_from_directory(input$dir,
+                                                        file_ext = input$ext, 
+                                                        hide_empty_dirs = input$hide_empty_dirs)),
                   plugins = input$plugins)
   )
   output$msg <- renderPrint( {
